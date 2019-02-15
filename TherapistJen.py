d4e69c3fe12bf5a7698d10eb68517d2fe1b417b6
Ethan Godden <ethan.godden@gmail.com>
2019-02-15 10:07:10 -0800

    Other spencer's changes

--- TherapistJen.py
+++ TherapistJen.py
@@ -1,5 +1,7 @@
-
+import os
 import cherrypy
+
+PATH = os.path.abspath(os.path.dirname(__file__))
 
 class Root(object):
     @cherrypy.expose
@@ -8,4 +10,13 @@ class Root(object):
 
 
 if __name__ == '__main__':
-    cherrypy.quickstart(Root(), '/')
+    config={
+        '/': {
+                'tools.staticdir.on': True,
+                'tools.staticdir.dir': PATH,
+                'tools.staticdir.index': 'index.html',
+            },
+    }
+    cherrypy.quickstart(Root(), '/', config)
+
+cherrypy.quickstart()
