f5e03c1f12cc7002ffb1d69245ee784712e6ce98
Ethan Godden <ethan.godden@gmail.com>
2019-02-16 16:45:23 -0800

    POST request skeleton done. Also, figured out why there was a venv folder so included that again

--- TherapistJen.py
+++ TherapistJen.py
@@ -3,19 +3,25 @@ import cherrypy
 
 PATH = os.path.abspath(os.path.dirname(__file__))
 
+
 class Root(object):
     @cherrypy.expose
     def index(self):
         return open('static/index.html')
 
 
+    @cherrypy.expose
+    def therapistJenResponce(self):
+        return "Therapist Jen is still at school getting her degree in Psychology"
+
+
 if __name__ == '__main__':
-    config={
+    config = {
         '/': {
-                'tools.staticdir.on': True,
-                'tools.staticdir.dir': PATH,
-                'tools.staticdir.index': 'index.html',
-            },
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': PATH,
+            'tools.staticdir.index': 'index.html',
+        },
     }
     cherrypy.quickstart(Root(), '/', config)
 
