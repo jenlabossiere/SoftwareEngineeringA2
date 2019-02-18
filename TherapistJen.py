fc2083c75d8f2583f80658ec44dba0f412d0ee7a
Spencer Ke <kespencer88@gmail.com>
2019-02-17 18:53:55 -0800

    put therapistHen changes into separate file

--- TherapistJen.py
+++ TherapistJen.py
@@ -1,15 +1,11 @@
 import os
-import databaseQueryTechnique
 import cherrypy
 
 PATH = os.path.abspath(os.path.dirname(__file__))
 
-sOrQ = "statement"
-feeling = "nothing"
-subject = "normal"
-questionNum = 1
 
 class Root(object):
+    # display the index.html
     @cherrypy.expose
     def index(self):
         return open('static/index.html')
@@ -17,24 +13,14 @@ class Root(object):
     # Method that is invoked on a post request. The .ajax in index.html points to this method
     @cherrypy.expose
     def therapistJenResponce(self):
-        global sOrQ
-        global feeling
-        global subject
-        global questionNum
-
-        if questionNum != 1:
-            databaseQueryTechnique.getResponse(sOrQ, self, subject, questionNum)
-        response = databaseQueryTechnique.getResponse(sOrQ, self, subject, questionNum)
-        return response
-
-
+        return "Therapist Jen is still at school getting her degree in Psychology"
 
 if __name__ == '__main__':
-    config={
+    config = {
         '/': {
-                'tools.staticdir.on': True,
-                'tools.staticdir.dir': PATH,
-                'tools.staticdir.index': 'index.html',
-            },
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': PATH,
+            'tools.staticdir.index': 'index.html',
+        },
     }
-    cherrypy.quickstart(Root(), '/', config)
+    cherrypy.quickstart(Root(), '/', config)  # start up the server
