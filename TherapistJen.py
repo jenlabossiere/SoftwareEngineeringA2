d122e8be0d7a4a38ba89284e4c21418b82a4b5aa
lindstrum <38596326+lindstrum@users.noreply.github.com>
2019-02-17 17:28:59 -0800

    Add files via upload

--- TherapistJen.py
+++ TherapistJen.py
@@ -1,11 +1,15 @@
 import os
+import databaseQueryTechnique
 import cherrypy
 
 PATH = os.path.abspath(os.path.dirname(__file__))
 
+sOrQ = "statement"
+feeling = "nothing"
+subject = "normal"
+questionNum = 1
 
 class Root(object):
-    # display the index.html
     @cherrypy.expose
     def index(self):
         return open('static/index.html')
@@ -13,14 +17,25 @@ class Root(object):
     # Method that is invoked on a post request. The .ajax in index.html points to this method
     @cherrypy.expose
     def therapistJenResponce(self):
-        return "Therapist Jen is still at school getting her degree in Psychology"
+        global sOrQ
+        global feeling
+        global subject
+        global questionNum
+
+        if questionNum != 1:
+            feeling = databaseQueryTechnique.getFeeling(self)
+            databaseQueryTechnique.getResponse(sOrQ, feeling, subject, questionNum)
+        response = databaseQueryTechnique.getResponse(sOrQ, feeling, subject, questionNum)
+        return response
+
+
 
 if __name__ == '__main__':
-    config = {
+    config={
         '/': {
-            'tools.staticdir.on': True,
-            'tools.staticdir.dir': PATH,
-            'tools.staticdir.index': 'index.html',
-        },
+                'tools.staticdir.on': True,
+                'tools.staticdir.dir': PATH,
+                'tools.staticdir.index': 'index.html',
+            },
     }
-    cherrypy.quickstart(Root(), '/', config)  # start up the server
+    cherrypy.quickstart(Root(), '/', config)
