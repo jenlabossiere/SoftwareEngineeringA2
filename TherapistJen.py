4d42dcfdd91d26584a0c27f67954a711d86143ef
Spencer Ke <kespencer88@gmail.com>
2019-02-17 17:56:58 -0800

    get NN working in TheapistJen

--- TherapistJen.py
+++ TherapistJen.py
@@ -1,11 +1,41 @@
-
+import os
+import dataBaseQueryTechnique
 import cherrypy
+
+PATH = os.path.abspath(os.path.dirname(__file__))
+
+sOrQ = "statement"
+feeling = "nothing"
+subject = "normal"
+questionNum = 1
 
 class Root(object):
     @cherrypy.expose
     def index(self):
         return open('static/index.html')
 
+    # Method that is invoked on a post request. The .ajax in index.html points to this method
+    @cherrypy.expose
+    def therapistJenResponce(self):
+        global sOrQ
+        global feeling
+        global subject
+        global questionNum
+
+        if questionNum != 1:
+            feeling = databaseQueryTechnique.getFeeling(self)
+            databaseQueryTechnique.getResponse(sOrQ, feeling, subject, questionNum)
+        response = databaseQueryTechnique.getResponse(sOrQ, feeling, subject, questionNum)
+        return response
+
+
 
 if __name__ == '__main__':
-    cherrypy.quickstart(Root(), '/')
+    config={
+        '/': {
+                'tools.staticdir.on': True,
+                'tools.staticdir.dir': PATH,
+                'tools.staticdir.index': 'index.html',
+            },
+    }
+    cherrypy.quickstart(Root(), '/', config)
